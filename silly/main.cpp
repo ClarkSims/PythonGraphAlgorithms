8e470659f2720d327c74c7a402126c070e5e5af3
Clark Sims <ClarkSims@acm.org>
2019-10-04 12:27:28 -0400

    clean up att oct3

--- silly/main.cpp
+++ silly/main.cpp
@@ -1,86 +1,48 @@
-//g++ -std=c++14 -g -o stub stub.cpp -l gtest -l pthread
-#include <algorithm>
-#include <assert.h>
-#include <bitset>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <deque>
-#include <fstream>
+//g++ -std=c++14 -g -o oct3 main.cpp -l gtest -l pthread
+#define gtest 0
+
+#if gtest
 #include <gtest/gtest.h>
+#endif
+
 #include <iostream>
-#include <limits>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdint.h>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
+#include <iomanip>
 #include <vector>
+#include <stdint.h>
+#include <cmath>
 
 using namespace std;
 
-#include <cmath>
-#include <gtest/gtest.h>
-
-typedef bool (*get_uint_t)(uint32_t*);
-
-void calc_sample_mean_smpl_var( get_uint_t get_uint, double& mean, double& smpl_var)
-{
-  uint32_t y, n;
-  uint64_t smy2=0, smy=0;
-  if (!get_uint(&n)) {
-    cerr << "can't read n" << endl;
-    exit(1);
+template<class internal_int = uint64_t, class arg_int = uint32_t>
+class online_normal_dist_estimator {
+  internal_int sum_y2 = 0;
+  internal_int sum_y = 0;
+  internal_int N = 0;
+public:
+  void update(arg_int y) {
+    ++N;
+    sum_y += y;
+    internal_int iy = y;
+    sum_y2 += iy * iy;
   }
-  for (uint32_t i = 0; i < n; ++i) {
-    if (!get_uint(&y)) {
-      cerr << "can't read y" << endl;
-      exit(1);
-    }
-    smy += y;
-    smy2 += y*y;
+  void get_estimators( double& mean, double& var) {
+    mean = (double)sum_y / N;
+    var = (double)sum_y2/(N-1) -  mean * mean * N / (N-1);
   }
-  mean = (double)smy / n;
-  smpl_var = (double)smy2/(n-1) -  mean * mean * n / (n-1);
-//  cerr << smpl_var << endl;
-}
-
-vector<uint32_t> data;
-int data_offset = -1;
-
-bool get_stdin(uint32_t* dat) {
-  uint32_t tmp;
-  cin >> tmp;
-  *dat = tmp;
-  return true;
-}
+};
 
 void stdin_main() {
   double m, s2;
-  calc_sample_mean_smpl_var(get_stdin, m, s2);
-}
-
-bool get_data(uint32_t* y) {
-  if (data_offset < 0) {
-    data_offset = 0;
-    *y = data.size();
-    return true;
+  online_normal_dist_estimator<> onde;
+  uint32_t N, tmp;
+  cin >> N;
+  for (unsigned i = 0; i < N; ++i) {
+    cin >> tmp;
+    onde.update(tmp);
   }
-  if (data_offset >= data.size()) {
-    return false;
-  }
-  *y = data.at(data_offset);
-  ++data_offset;
-  return true;
+  onde.get_estimators(m, s2);
+  cout << setprecision(9) << m << endl;
+  cout << setprecision(9) << s2 << endl;
 }
 
 bool double_equal(double lhs, double rhs, double eps) {
@@ -90,19 +52,28 @@ bool double_equal(double lhs, double rhs, double eps) {
   return (numer / denom) < eps;
 }
 
+#if gtest
 TEST(simple, one) {
   vector<uint32_t> tdata{ 1, 2, 3, 4};
-  data.swap(tdata);
+  online_normal_dist_estimator<> onde;
+
+  for (unsigned i = 0; i < tdata.size(); ++i) {
+    onde.update(tdata.at(i));
+  }
   double m, s2;
-  calc_sample_mean_smpl_var(get_data, m, s2);
+  onde.get_estimators(m, s2);
+
   EXPECT_TRUE(double_equal(m, 2.5, .0001));
   EXPECT_TRUE(double_equal(s2, 1.666667, .001));
 }
-
+#endif
 
 int main(int argc, char **argv) {
-  //testing::InitGoogleTest(&argc, argv);
-  //return RUN_ALL_TESTS();
+#if gtest
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+#else
   stdin_main();
   return 0;
+#endif
 }
